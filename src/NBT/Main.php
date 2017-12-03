58ce83f1255fc384fa5105fcd467ed9645c658af
Angel <bubblicious712@gmail.com>
2017-12-03 01:33:03 -0500

    Create Main.php

---
+++ src/NBT/Main.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace NBT;
+
+use pocketmine\event\Listener;
+use pocketmine\plugin\PluginBase;
+
+use pocketmine\Player;
+
+use pocketmine\event\player\{PlayerJoinEvent, PlayerInteractEvent};
+
+use pocketmine\nbt\tag\StringTag;
+
+class Main extends PluginBase implements Listener{
+}
